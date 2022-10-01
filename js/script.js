43203521d322131edb11d78b03f49bb86f6ebd99
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-10-01 18:40:42 +0300

    created new project
    making tabs active, switch between tabs

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+
+    tabs[i].classList.add("tabheader__item_active");
+  }
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          // item - это каждый tab в переборе, i - это номер элемента по порядку в массиве
+
+          hideTabContent();
+          showTabContent(i); // если пользователь кликнул на 3й таб - откроется меню для 3 таба и тд
+        }
+      });
+    }
+  });
+});
