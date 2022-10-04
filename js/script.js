1f16e433391faa9fdf1adea92a89bdeab61d431b
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-10-04 15:23:17 +0300

    added dynamic windows content, using classes

--- js/script.js
+++ js/script.js
@@ -163,4 +163,67 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   window.addEventListener("scroll", showModalByScroll);
   //показать модалку, когда пользователь долистал до конца страницы
+
+  //Dynamic content with classes for cards
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.parent = document.querySelector(parentSelector); //элемент, в который мы будем размещать карточку
+      this.price = price;
+      this.transfer = 36;
+      this.changeToUAH(); //конвертация в грн
+    }
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+         <div class="menu__item">
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Ціна:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+          </div>
+      `;
+      this.parent.append(element); //разместить элемент внутри родителя
+    }
+  }
+
+  //создаем объект и вызываем рендер
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фітнес"',
+    'Меню "Фітнес" - це новий підхід к приготуванню блюд: більше свіжих овочів та фруктів. Продукт активних та здорових людей. Це абсолютно новий продукт з оптимальною ценою та високою якістю!',
+    15,
+    ".menu .container" //родительский селектор
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Пісне"',
+    "Меню “Пісне” - це ретельний відбір інгредієнтів: відсутність продуктів тваринного походження, молоко з мигдаля, вівса, кокоса або гречки, вірна кількість білків за рахунок тофу і вегетарианських стейків.",
+    14,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    "Меню “Преміум”",
+    "В меню “Преміум” ми використовуємо не лише красивий дизайн пакування, але й якісне виконання блюд. Червона риба, морепродукти, фрукти - ресторанне меню без походу до ресторану!",
+    21,
+    ".menu .container"
+  ).render();
 });
